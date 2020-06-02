0fce38a58e42ecd4bad9a00a0ccd81ba0641bba6
Anthony Sellitti <anthony.sellitti@envato.com>
2020-06-02 10:28:20 +1000

    Add more waits in place because js

--- keyboard/ergodox/keymap_macros.h
+++ keyboard/ergodox/keymap_macros.h
@@ -9,10 +9,10 @@ END) \
 
 #define MACRO_DEV_CREDIT_CARD MACRO( \
     I(5), \
-    T(4), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(TAB), \
-    W(100), T(1), T(TAB), \
-    W(100), T(2), T(2), T(TAB), \
-    W(100), T(3), T(3), T(3), \
+    T(4), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), T(1), \
+    W(100), T(TAB), W(100), T(1), \
+    W(100), T(TAB), W(100), T(2), T(2), \
+    W(100), T(TAB), W(100), T(3), T(3), T(3), \
 END) \
 
 #define MACRO_QUIT_VIM MACRO( \
